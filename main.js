64463de2a39b155a98fdcc3de09b151c99da44bb
azizbek400 <aziznormurodov400@gmail.com>
2023-12-07 22:41:32 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// 1-masala
+let a = 10;
+let b = 3;
+let c = a + b;
+let d = c*2;
+
+console.log(d);
+
+
+// 2-masala
+let num = 43;
+let num2 = 121;
+let numbers = num + num2;
+
+console.log(numbers);
